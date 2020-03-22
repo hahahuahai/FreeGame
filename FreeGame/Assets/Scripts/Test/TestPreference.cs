a32071642a537e4534fa7e6c0fdab23c6a1b2466
huahai <279598616@qq.com>
2020-03-22 20:11:12 +0800

    添加Preferences相关的测试代码。

---
+++ FreeGame/Assets/Scripts/Test/TestPreference.cs
@@ -0,0 +1,32 @@
+﻿using Loxodon.Framework.Prefs;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestPreference : MonoBehaviour
+{
+    private void Awake()
+    {
+ 
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        BinaryFilePreferencesFactory factory = new BinaryFilePreferencesFactory();
+        Preferences.Register(factory);
+
+        Preferences preferences = Preferences.GetGlobalPreferences();
+        bool b = preferences.ContainsKey("ChinesePeople");
+        Debug.Log("名为ChinesePeople的Preferences数据是否存在:" + b);
+        preferences.SetString("ChinesePeople", "lilei");
+        preferences.Save();
+        Debug.Log("存储Preferences数据的地址Application.persistentDataPath为:" + Application.persistentDataPath);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
